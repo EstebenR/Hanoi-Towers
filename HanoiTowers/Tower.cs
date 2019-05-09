d735a431cb1aa35338dbe1e927f3658bf13545f6
Esteban <35503563+EstebenR@users.noreply.github.com>
2019-05-09 18:12:30 +0200

    Created Project, Disc and Tower class

---
+++ HanoiTowers/Tower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTowers
+{
+    public class Tower
+    {
+        class Disc
+        {
+            int size;
+            Disc above, below;
+
+            public Disc(int discSize)
+            {
+                size = discSize;
+                above = null; below = null;
+            }
+
+            public Disc(int discSize, Disc discBelow)
+            {
+                size = discSize;
+                above = discBelow;
+            }
+        }
+
+
+        public Tower()
+        {
+
+        }
+    }
+}
